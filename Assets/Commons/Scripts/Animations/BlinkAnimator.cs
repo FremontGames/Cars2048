418588c34e28889a2fe6c2f1956342d616260284
Damien Frémont <damien.fremont@gmail.com>
2021-09-15 23:13:36 -0800

    script: common animation

---
+++ Assets/Commons/Scripts/Animations/BlinkAnimator.cs
@@ -0,0 +1,54 @@
+﻿/* Copyright (C) 2017 Damien Fremont - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary
+ * Written by Damien Fremont
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Commons.Animations
+{
+    public class BlinkAnimator : MonoBehaviour
+    {
+       // static float delayInSeconds = 1f;
+        public float[][] transitionsFadeDelay;
+
+        public BlinkAnimator()
+        {
+            transitionsFadeDelay = new float[7][];
+            transitionsFadeDelay[0] = new float[] { .4f, 1.5f };
+            transitionsFadeDelay[1] = new float[] { .5f, .05f };
+            transitionsFadeDelay[2] = new float[] { .6f, .05f };
+            transitionsFadeDelay[3] = new float[] { .7f, .05f };
+            transitionsFadeDelay[4] = new float[] { .8f, .05f };
+            transitionsFadeDelay[5] = new float[] { .9f, .1f };
+            transitionsFadeDelay[6] = new float[] { 1f, .1f };
+        }
+
+        void Start()
+        {
+            StartCoroutine(Coroutine(gameObject));
+        }
+
+        private IEnumerator Coroutine(GameObject go)
+        {
+            SpriteRenderer sprite = go.GetComponent<SpriteRenderer>();
+            while (true)
+            {
+                foreach (float[] trans in transitionsFadeDelay)
+                {
+                    sprite.color = new Color(1f, 1f, 1f, trans[0]);
+                    yield return new WaitForSeconds(trans[1]);
+                }
+                foreach (float[] trans in transitionsFadeDelay.Reverse<float[]>())
+                {
+                    sprite.color = new Color(1f, 1f, 1f, trans[0]);
+                    yield return new WaitForSeconds(trans[1]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Commons/Scripts/Animations/MoveInTime.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Commons.Animations
+{
+    public class MoveInTime : MonoBehaviour
+    {
+        public Vector3 begin;
+        public Vector3 end;
+        public float time_for_move = 0.7f;
+
+        protected float timer;
+
+        protected void Start()
+        {
+            transform.position = begin;
+            timer = time_for_move;
+        }
+
+        protected void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer > 0)
+            {
+                Vector3 distance = end - begin;
+                float degree_of_movement = (time_for_move - timer) / time_for_move;
+                transform.position = new Vector3(
+                    begin.x + (distance.x * degree_of_movement),
+                    begin.y + (distance.y * degree_of_movement),
+                    begin.z + (distance.z * degree_of_movement)
+                    );
+            }
+        }
+    }
+
+    public class MoveInTimeThenDestroy : MoveInTime
+    {
+        new void Update()
+        {
+            base.Update();
+            if (timer <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+    public class MoveInTimeDirectionThenDestroy : MoveInTimeDirection
+    {
+        new void Update()
+        {
+            base.Update();
+            if (timer <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+    public class MoveInTimeDirection : MoveInTime
+    {
+        public Vector3 move = new Vector3(0, 100, 0);
+
+        new void Start()
+        {
+            begin = transform.position;
+            end = new Vector3(
+                begin.x + move.x,
+                begin.y + move.y,
+                begin.z + move.z);
+            base.Start();
+        }
+    }
+}
